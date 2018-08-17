59e8ff868fd7503edc90a0f375f8b6510601b732
CashewRose <danieagnoletti@gmail.com>
2018-08-17 13:38:47 -0500

    fixed import issue

--- website/urls.py
+++ website/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     url(r'^login$', views.login_user, name='login'),
     url(r'^logout$', views.user_logout, name='logout'),
     url(r'^register$', views.register, name='register'),
-    url(r'^sell$', views.sell_product, name='sell'),
-    url(r'^products$', views.list_products, name='list_products'),
+    # url(r'^sell$', views.sell_product, name='sell'),
+    # url(r'^products$', views.list_products, name='list_products'),
 ]
