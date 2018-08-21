b3fa0fcaa7432e9b0e24b9a2f095f30d3f31ac83
JacobAndrewSmith92 <jacobandrewsmith92@gmail.com>
2018-08-21 15:54:11 -0500

    Adding basic files for payment type and options

---
+++ website/views/payment_option_view.py
@@ -0,0 +1,8 @@
+
+def payment_option(request):
+"""payment_option wil handle rendering the form view for payment option when the 'My Account page renders'
+    It will require a form that has an affordance for:
+        Card Name: <input field> Ex. 'My Airline Rewards Card' or 'My Ascend Credit Union Card'
+        Payment Type: <dropdown> Ex. Visa, Mastercard, etc.
+        Account #: <input field> Ex. 54310
+"""
