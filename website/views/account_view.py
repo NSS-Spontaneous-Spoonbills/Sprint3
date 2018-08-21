261729c5533477f49572bb80614f19e6bf73871b
CashewRose <danieagnoletti@gmail.com>
2018-08-21 13:05:42 -0500

    Will redirect to the login page if you are not logged in.

--- website/views/account_view.py
+++ website/views/account_view.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
-@login_required(redirect_field_name='login')
+@login_required()
 def my_account_home(request):
     """[summary]
     
@@ -9,8 +10,10 @@ def my_account_home(request):
         request {[type]} -- [description]
     """
 
-
     return render(request, 'account/my_account_index.html', {})
 
 
+# def wall(request):
+
+#     return HttpResponse("Invalid login details supplied.")
 
