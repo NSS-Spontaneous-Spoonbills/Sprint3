949c8466f6d9aa940b7d8d1fd5db6fd59b31828f
David Paul <dfp333@gmail.com>
2018-08-22 09:31:18 -0500

    added the stuff to the urls or like, whatever man

--- website/urls.py
+++ website/urls.py
@@ -13,6 +13,6 @@ urlpatterns = [
     # url(r'^sell$', views.sell_product, name='sell'),
     # url(r'^products$', views.list_products, name='list_products'),
     url(r'^product_types$', views.Product_Type_List_View, name='product_types'),
-    url(r'^product_category$', view.Product_Category_View, name='product_category'),
+    url(r'^product_category$', views.Product_Category_View, name='product_category'),
 ]
 
--- website/views/__init__.py
+++ website/views/__init__.py
@@ -1,2 +1,2 @@
-from .Product_Type_View import Product_Type_List_View
+from .Product_Type_View import Product_Type_List_View, Product_Category_View
 from .auth_view import index, login_user, register, user_logout
